bb0f0a45bd6b0891d1ebfb354841ef52d3f3e681
Vipul Chadda <vipul.chadda@gmail.com>
2020-10-05 22:19:35 -0400

    Removing OnInit

--- ui/src/app/components/log-button/log-button.component.ts
+++ ui/src/app/components/log-button/log-button.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
 import { ActivityType } from 'src/app/enums/activity-type.enum';
 import { ApiService } from 'src/app/services/api.service';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-log-button',
     templateUrl: './log-button.component.html',
     styleUrls: ['./log-button.component.scss']
 })
-export class LogButtonComponent implements OnInit {
+export class LogButtonComponent {
     @Input() type: ActivityType;
 
     constructor(private apiService: ApiService) {}
-
-    ngOnInit(): void {}
 
     click() {
         this.apiService.trackActivity(this.type);
