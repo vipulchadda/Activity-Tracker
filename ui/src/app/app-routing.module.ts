9e4e9cf68d6a1682ebb45fe900281f0209e70547
Vipul Chadda <vipul.chadda@gmail.com>
2020-10-05 20:58:20 -0400

    Adding cloudbuild.json for server Updating routes to use index.html

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: 'index.html', pathMatch: 'full' },
+    { path: 'index.html', component: AppComponent }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
